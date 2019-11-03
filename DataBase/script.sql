523ebe31a73c463e13609d251fed1155220af926
Ania <anula.le@wp.pl>
2019-11-03 15:18:55 +0100

    products - virtual table for full text search

--- DataBase/script.sql
+++ DataBase/script.sql
@@ -9,11 +9,69 @@ create table Products
     carbs              String,
     fats               String,
     onePortionInGrams  real,
-    userId             int
+    userId             int,
+    allergens          String
 );
 
 create unique index Products_id_uindex
     on Products (id);
+
+CREATE TRIGGER ProductsFts_Delete
+    AFTER DELETE
+    ON Products
+BEGIN
+    Delete from ProductsFts where id = old.id;
+END;
+
+CREATE TRIGGER ProductsFts_Insert
+    AFTER INSERT
+    ON Products
+BEGIN
+    INSERT INTO ProductsFts(id, name, caloriesIn100Grams, protein, carbs, fats, onePortionInGrams, userId, allergens)
+    VALUES (new.id, new.name, new.caloriesIn100Grams, new.protein, new.carbs, new.fats, new.onePortionInGrams,
+            new.userId, new.allergens);
+END;
+
+CREATE TRIGGER ProductsFts_Update
+    AFTER UPDATE
+    ON Products
+BEGIN
+    Delete from ProductsFts where id = old.id;
+    INSERT INTO ProductsFts(id, name, caloriesIn100Grams, protein, carbs, fats, onePortionInGrams, userId, allergens)
+    VALUES (new.id, new.name, new.caloriesIn100Grams, new.protein, new.carbs, new.fats, new.onePortionInGrams,
+            new.userId, new.allergens);
+END;
+
+create table ProductsFts_config
+(
+    k not null
+        primary key,
+    v
+)
+    without rowid;
+
+create table ProductsFts_data
+(
+    id    INTEGER
+        primary key,
+    block BLOB
+);
+
+create table ProductsFts_docsize
+(
+    id INTEGER
+        primary key,
+    sz BLOB
+);
+
+create table ProductsFts_idx
+(
+    segid not null,
+    term  not null,
+    pgno,
+    primary key (segid, term)
+)
+    without rowid;
 
 create table Users
 (
@@ -54,6 +112,21 @@ create table SpecificProductForMeal
         references Meal,
     constraint SpecificProductForMeal_pk
         primary key (productId, mealId)
+);
+
+create virtual table ProductsFts using fts5
+(
+    id,
+    name,
+    caloriesIn100Grams,
+    protein,
+    carbs,
+    fats,
+    onePortionInGrams,
+    userId,
+    allergens,
+    content=
+    'Products'
 );
 
 
