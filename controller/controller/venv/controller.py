afe1228c4e0c99922d03b73d16234c8f7542d84b
Weronika <weronika.kowalska@student.put.poznan.pl>
2019-11-06 12:26:28 +0100

    structuring and example database

--- controller/controller/venv/controller.py
+++ controller/controller/venv/controller.py
@@ -1,5 +1,29 @@
 import sqlite3
 
-# creating a connection to database given in a file
-sqlite_file = "path"
-conn = sqlite3.connect(sqlite_file)
+def connect_to_db(sqlite_file):
+    conn = None
+    conn = sqlite3.connect(sqlite_file)
+    print("conected")
+    return conn
+
+def create_to_db(query, conn):
+    c = conn.cursor()
+    c.execute(query)
+
+def read_to_db(query, conn):
+    c = conn.cursor()
+    c.execute(query)
+
+def update_to_db(query, conn):
+    c = conn.cursor()
+    c.execute(query)
+
+def delete_to_db(query, conn):
+    c = conn.cursor()
+    c.execute(query)
+
+if __name__ == '__main__':
+    database = "skrypt-kopia.sqlite"
+    conn = connect_to_db(database)
+
+
