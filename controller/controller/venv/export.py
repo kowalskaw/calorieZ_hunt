660e94826a1accf2b699cedb3c6b7105782f5b02
sowiacz <38477303+sowiacz@users.noreply.github.com>
2019-12-17 20:29:20 +0100

    Export database to JSON

---
+++ controller/controller/venv/export.py
@@ -0,0 +1,80 @@
+import json
+import sqlite3
+
+from users_controller import Users
+from products_controller import Products
+
+
+class Export:
+    def __init__(self, conn, cursor):
+        self.conn = conn
+        self.cursor = cursor
+
+    def get_users(self):
+        query = '''
+        SELECT * FROM Users
+        '''
+        self.cursor.execute(query)
+        data = self.cursor.fetchall()
+        list = Users.user_tuple_to_dict_with_id(self, data)
+        return list
+
+    def get_products(self):
+        query = '''
+        SELECT * FROM Products
+        '''
+        self.cursor.execute(query)
+        data = self.cursor.fetchall()
+        list = Products.product_tuple_to_dict_with_id(self, data)
+        return list
+
+    def get_meals(self):
+        query = '''
+        SELECT * FROM Meal
+        '''
+        self.cursor.execute(query)
+        data = self.cursor.fetchall()
+        list = []
+        for tuple in data:
+            user_as_dict = {
+                'id': tuple[0],
+                'mealType': tuple[1],
+                'date': tuple[2],
+                'userId': tuple[3]
+            }
+            list.append(user_as_dict)
+        return list
+
+    def get_specific_products_for_meals(self):
+        query = '''
+        SELECT * FROM SpecificProductForMeal
+        '''
+        self.cursor.execute(query)
+        data = self.cursor.fetchall()
+        list = []
+        for tuple in data:
+            user_as_dict = {
+                'productId': tuple[0],
+                'mealId': tuple[1],
+                'portionInGrams': tuple[2]
+            }
+            list.append(user_as_dict)
+        return list
+
+
+def export_data():
+    conn = sqlite3.connect("skrypt.db")
+    cursor = conn.cursor()
+    toExport = Export(conn, cursor)
+
+    file = open("data.json", "w")
+    file.write(json.dumps({"Users": toExport.get_users()}, sort_keys=True, indent=4))
+    file.write(json.dumps({"Products": toExport.get_products()}, sort_keys=True, indent=4))
+    file.write(json.dumps({"Meal": toExport.get_meals()}, sort_keys=True, indent=4))
+    file.write(
+        json.dumps({"SpecificProductForMeal": toExport.get_specific_products_for_meals()}, sort_keys=True, indent=4))
+    file.close()
+
+
+if __name__ == '__main__':
+    export_data()
