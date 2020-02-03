01968ca51773366d1a34e383acae721de131637b
Ania <anula.le@wp.pl>
2020-02-03 13:35:07 +0100

    testy

--- tests/tests/addProducts.test.js
+++ tests/tests/addProducts.test.js
@@ -1,13 +1,12 @@
 const axios = require('axios');
 describe('GET Product', function() {
-    it('should get a product object', function(){
-    //    axios.get('/product?id=4')
-    //        .then((res) => {
-    //            console.log(res.status)
-    //            expect(res.status).toBe(200);
-    //        })
-    //        .catch((e) => {
-    //            console.log(e);
-    //        });
-   })
+    it('should get a product object',()=>{
+        axios.get('http://192.168.99.100:5678/product?id=4')
+        .then((res) => {
+            expect(res.status).toBe(200);
+        })
+        .catch((e) => {
+        });
+        
+    })
 })
--- tests/tests/addUser.test.js
+++ tests/tests/addUser.test.js
@@ -1,43 +1,32 @@
 const axios = require('axios');
 
-// describe('Users', () => {
-//     describe('POST User', () => {
-//         it('should create an user object', () => {
-//             const user = {
-//                 'id': '55',
-//                 'first_name': 'Dominik',
-//                 'last_name': 'XYZ',
-//                 'email': 'nanaxn@wp.pl',
-//                 'password': 'password',
-//                 'sex': '1',
-//                 'weight': '60',
-//                 'height': '170',
-//                 'allergies': '0',
-//                 'calories_intake_daily': '1222',
-//                 'weight_goal': '60',
-//                 'user_name': 'lupinka',
-//                 'birthDate': '4.12.1998'
-//             };
-
-//             axios.post('http://192.168.99.100:5678/user', user)
-//                 .then((res) => {
-//                     expect(res.status).toBe(201);
-//                 })
-//                 .catch((e) => {
-//                     //console.log(e);
-//                 });
-//         });
-//     });
+describe('Users', () => {
+    describe('POST User', () => {
+        it('should create an user object', () => {
+            axios.post('http://192.168.99.100:5678/user?password=122&first_name='+
+            'qqws&last_name=aSSA&email=ZXZX&sex=1&weight=55&height=170&allergies='+
+            '-&calories_intake_daily=1111&weight_goal=60&user_name=lupinka&birthDate=4.12.1998')
+                .then((res) => {
+                    expect(res.status).toBe(201);
+                })
+                .catch((e) => {
+                    //console.log(e);
+                });
+        });
+    });
 
     describe('GET User', function() {
-         it('should get an user object', function(){
-            axios.get('/user?id=4')
+        var i
+        for(i=1;i<100000;i++){
+            it('should get an user object',()=>{
+                axios.get('http://192.168.99.100:5678/user?id=4')
                 .then((res) => {
-                    console.log(res.status)
                     expect(res.status).toBe(200);
                 })
                 .catch((e) => {
-                    console.log(e);
                 });
-        })
+                
+            })
+        }
+         
     })
